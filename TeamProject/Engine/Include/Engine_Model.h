76c97106ae5ee4692535cd076f7aa967d166837a
tballoon23 <asbssu18@gmail.com>
2025-07-01 18:10:19 +0900

    refactor : Engine_Model.h

--- TeamProject/Engine/Include/Engine_Model.h
+++ TeamProject/Engine/Include/Engine_Model.h
@@ -6,30 +6,37 @@
 #include "CMaterial.h"
 #include "CModel.h"
 
-struct CubeModel
+struct DefaultCubeModel
 {
 	wstring meshKey = L"CCubeTex";
 	wstring textureKey = L"brick.png";
 	wstring materialKey = L"metal";
 };
 
-struct MeshKey { wstring value; };
-struct TextureKey { wstring value; };
-struct MaterialKey { wstring value; };
+struct MeshKey { const wstring& value; };
+struct TextureKey { const wstring& value; };
+struct MaterialKey { const wstring& value; };
 
-template <typename TArg> void ApplyArg(DefaultCubeModel& model, TArg arg)
+template <typename TArg>
+void ApplyArg(DefaultCubeModel& model, TArg arg)
 {    
 	// 기본은 아무 것도 하지 않음 
 }
-template <> void ApplyArg(DefaultCubeModel & model, MeshKey arg)
+
+template <>
+void ApplyArg(DefaultCubeModel & model, MeshKey arg)
 {
 	model.meshKey = arg.value;
 }
-template <> void ApplyArg(DefaultCubeModel & model, TextureKey arg)
+
+template <>
+void ApplyArg(DefaultCubeModel & model, TextureKey arg)
 {
 	model.textureKey = arg.value;
 }
-template <> void ApplyArg(DefaultCubeModel & model, MaterialKey arg)
+
+template <>
+void ApplyArg(DefaultCubeModel & model, MaterialKey arg)
 {
 	model.materialKey = arg.value;
 }
