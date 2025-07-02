6ed20e0931db9b10763b65da562210556d39e55c
tballoon23 <asbssu18@gmail.com>
2025-07-02 20:34:20 +0900

    feat : commit #44
    키보드와 마찬가지로 마우스의 입력상태를 구분할수있는 함수를 추가하였습니다.

--- TeamProject/Engine/Header/CInputMgr.h
+++ TeamProject/Engine/Header/CInputMgr.h
@@ -24,6 +24,12 @@ public:
 	_bool Key_Release(int vkKey) { return!(m_CurByKeyState[vkKey] & 0x80); }
 
 public:
+	_bool	Mouse_Tap(MOUSEKEYSTATE eMouse);
+	_bool	Mouse_Hold(MOUSEKEYSTATE eMouse);
+	_bool	Mouse_Down(MOUSEKEYSTATE eMouse);
+	_bool	Mouse_Away(MOUSEKEYSTATE eMouse);
+
+public:
 	_long	Get_DIMouseMove(MOUSEMOVESTATE eMouseState);
 	_byte	Get_DIMouseState(MOUSEKEYSTATE eMouse);
 
@@ -37,7 +43,7 @@ private:
 private:
 	_byte					m_CurByKeyState[256];		// 키보드에 있는 모든 키값을 저장하기 위한 변수 (현재 프레임)
 	_byte					m_PrevByKeyState[256];		// 키보드에 있는 모든 키값을 저장하기 위한 변수(이전 프레임)
-	DIMOUSESTATE			m_tMouseState; //마우스 상태
+	DIMOUSESTATE			m_tCurMouseState; //마우스 상태
 	DIMOUSESTATE			m_PreMouseState; //마우스 상태
 
 private:
