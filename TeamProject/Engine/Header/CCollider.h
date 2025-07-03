7953fedf09fd10320b21ddc37e9feb43f882e688
tballoon23 <asbssu18@gmail.com>
2025-07-03 17:02:33 +0900

    feat : CCollider #56
    충돌을 위한 컴포넌트를 생성하고 디버깅 시각화를 위해 렌더매니저에 등록하였습니다.
    충돌로직은 충돌매니저에서 추후 구현

---
+++ TeamProject/Engine/Header/CCollider.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "CComponent.h"
+
+BEGIN(Engine)
+
+class ENGINE_DLL CCollider : public CComponent
+{
+protected:
+	explicit CCollider(AABB aabb);
+
+public:
+	virtual  ~CCollider();
+
+public:
+	static CCollider* Create(AABB aabb);
+
+public:
+	virtual void Update_Component(const _float& fTimeDelta) override;
+	virtual void LateUpdate_Component() override;
+	void Render(LPDIRECT3DDEVICE9 pDevice);
+	virtual void Free();
+
+private:
+	AABB m_tAABB;
+
+	LPDIRECT3DVERTEXBUFFER9 m_pVB = nullptr;
+	LPDIRECT3DINDEXBUFFER9  m_pIB = nullptr;
+};
+
+END
+
