89f5321ec70cba9c48cc9fa02c4f6bfe4c8adbed
clean-owl <hunwoo7@naver.com>
2025-07-07 15:11:09 +0900

    feat : DirectionalCube Added
    방향성 큐브 기초 틀 만듦 나중에 기능 추가할 가능성 높음

--- TeamProject/Client/Header/CDirectionalCube.h
+++ TeamProject/Client/Header/CDirectionalCube.h
@@ -1,8 +1,52 @@
 #pragma once
 #include "CCube.h"
+
+namespace Engine {
+	class CCollider;
+	class CPickTarget;
+}
 class CDirectionalCube : public CCube
 {
 private:
+	explicit CDirectionalCube(LPDIRECT3DDEVICE9 pGraphicDev);
+	explicit CDirectionalCube(const CDirectionalCube& rhs);
+	virtual ~CDirectionalCube();
+
+public:
+	//Basic Function
+	HRESULT Ready_GameObject()override;
+	_int Update_GameObject(const _float& fTimeDelta) override;
+	void LateUpdate_GameObject(const _float& fTimeDelta) override;
+
+	//Create, Release Function
+	static CDirectionalCube* Create(LPDIRECT3DDEVICE9 pGraphicDev);
+	void Free() override;
+
+	//Getter, Setter Function
+	void Set_CursorVec(const _vec3& cursorvec) { m_vCursorDelta = cursorvec; }
+	void Set_Info(const _vec3& startpos,  const _vec3& axis, const _float& mindistance, const _float& maxdistance);
+	void Set_Grab(const bool& Trigger) { m_bGrab = Trigger; }
+
+private:
+	//Compute Function
+	void ComputeMoveVecIntoAxisMoveVec();
+	void ComputeMinMaxPos();
+	void Move();
+
+	//Variables
+	CCollider*		m_pCollider;
+	CPickTarget*	m_pPick;
+
+	_vec3			m_vStartPos;
+	_vec3			m_vDefaultAxis;
+	_vec3			m_vCursorDelta;
+	_vec3			m_vMoveDelta;
+	_vec3			m_vMinPos;
+	_vec3			m_vMaxPos;
+	_float			m_fMaxDistance;
+	_float			m_fMinDistance;
+
+	_bool			m_bGrab;
 
 };
 
