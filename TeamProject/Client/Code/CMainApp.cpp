962e94d62f26e30bc7e729de4e829bf9598bb3ca
tballoon23 <asbssu18@gmail.com>
2025-07-10 14:09:54 +0900

    refactor : Mgr class #122

--- TeamProject/Client/Code/CMainApp.cpp
+++ TeamProject/Client/Code/CMainApp.cpp
@@ -11,6 +11,7 @@
 #include "CLightMgr.h"
 #include "CPickingMgr.h"
 #include "CRenderMgr.h"
+#include "CUIMgr.h"
 #include "CCollisionMgr.h"
 #include "CCameraMgr.h"
 #include "CShaderMgr.h"
@@ -33,25 +34,32 @@ HRESULT CMainApp::Ready_MainApp()
 	if (FAILED(CGraphicDev::Get_Instance()->Ready_GraphicDev(g_hWnd, MODE_WIN, WINCX, WINCY, &m_pDeviceClass)))
 		return E_FAIL;
 
-	if (FAILED(CInputMgr::Get_Instance()->Ready_InputDev(g_HInst, g_hWnd)))
-		return E_FAIL;	
-
 	m_pDeviceClass->AddRef();
-
 	m_pGraphicDev = m_pDeviceClass->Get_GraphicDev();
 	m_pGraphicDev->AddRef();
 
+	CInputMgr::Get_Instance()->Ready_InputDev(g_HInst, g_hWnd);
 	CResourceMgr::Get_Instance()->Ready_Resource();
 	CShaderMgr::Get_Instance()->Ready_Shader(m_pGraphicDev);
 	CRenderMgr::Get_Instance()->Ready_RenderMgr();
+	CUiMgr::Get_Instance()->Ready_UiMgr();
+	CPickingMgr::Get_Instance()->Ready_Picking(m_pGraphicDev, g_hWnd);
+
+	// CameraMgr, LightMgr, CollisionMgr 초기화는 필요시 추가
+
 	m_pScene = Logo::Create(m_pGraphicDev);
 	CSceneMgr::Get_Instance()->Set_Scene(m_pScene);
+
 	return S_OK;
 }
 
 int CMainApp::Update_MainApp(_float& fTimeDelta)
 {
 	CInputMgr::Get_Instance()->Update_InputDev();
+	CPickingMgr::Get_Instance()->Update_Picking(fTimeDelta);
+	CCameraMgr::Get_Instance()->Update_Camera(m_pGraphicDev, fTimeDelta);
+	CCollisionMgr::Get_Instance()->Update_Collision();
+
 	CSceneMgr::Get_Instance()->Update_Scene(fTimeDelta);
 
 	return 0;
@@ -60,6 +68,10 @@ int CMainApp::Update_MainApp(_float& fTimeDelta)
 void CMainApp::LateUpdate_MainApp(_float& fTimeDelta)
 {
 	CInputMgr::Get_Instance()->LateUpdate_InputDev();
+	CPickingMgr::Get_Instance()->LateUpdate_Picking(fTimeDelta);
+	CCameraMgr::Get_Instance()->LateUpdate_Camera(fTimeDelta);
+	CCollisionMgr::Get_Instance()->LateUpdate_Collision();
+
 	CSceneMgr::Get_Instance()->LateUpdate_Scene(fTimeDelta);
 }
 
@@ -100,6 +112,7 @@ void CMainApp::Free()
 	CTimeMgr::Get_Instance()->Destroy_Instance();
 	CLightMgr::Get_Instance()->Destroy_Instance();
 	CPickingMgr::Get_Instance()->Destroy_Instance();
+	CUiMgr::Destroy_Instance();
 
 	// 4. 리소스, 디바이스
 	CResourceMgr::Get_Instance()->Destroy_Instance();
