b214d25734f9f8d16ce2c5543d4e0e75715d4f28
HHS0103 <hhs3528@naver.com>
2025-07-22 15:04:45 +0900

    feat : Stage3 임시저장 2.1(#202)
    Stage3 임시저장

--- TeamProject/Client/Code/SceneStage3.cpp
+++ TeamProject/Client/Code/SceneStage3.cpp
@@ -30,6 +30,7 @@
 #include "CSlotCube_Auto.h"
 #include "CPlayerTriggerCube.h"
 #include "CMirrorSlotCube.h"
+#include "CZoneSensor.h"
 
 #include "SceneSB.h"
 
@@ -108,8 +109,6 @@ void SceneStage3::FloatingSet()
 	for (int i = 1; i < 5; i++) {
 		wstring name = L"CDoor1_" + to_wstring(i);
 		CFloatingCube* cube = Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(name);
-		cube->Set_Loop();
-		cube->SetTrigger(true);
 		if (i == 1 || i == 3)
 		{
 			cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { -1.f, 0.f, 0.f }, 5.f, 5.f, 0.5f);
@@ -118,21 +117,23 @@ void SceneStage3::FloatingSet()
 		{
 			cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { 1.f, 0.f, 0.f }, 5.f, 5.f, 0.5f);
 		}
+		cube->Set_Loop();
+		cube->SetTrigger(true);
 	}
 
 	for (int i = 1; i < 8; i++) {
 		wstring name = L"CFloatingCube1_1_" + to_wstring(i);
 		CFloatingCube* cube = Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(name);
+		cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { 1.f, 0.f, 0.f }, 5.f, 5.f, 0.5f);
 		cube->Set_Loop();
 		cube->SetTrigger(true);
-		cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { 1.f, 0.f, 0.f }, 5.f, 5.f, 0.5f);
 	}
 	for (int i = 1; i < 8; i++) {
 		wstring name = L"CFloatingCube1_2_" + to_wstring(i);
 		CFloatingCube* cube = Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(name);
+		cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { -1.f, 0.f, 0.f }, 5.f, 5.f, 0.5f);
 		cube->Set_Loop();
 		cube->SetTrigger(true);
-		cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { -1.f, 0.f, 0.f }, 5.f, 5.f, 0.5f);
 	}
 
 	CFloatingCube* CFloatingCube1_1 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CFloatingCube1_1");
@@ -143,14 +144,13 @@ void SceneStage3::FloatingSet()
 	for (int i = 1; i < 13; i++) {
 		wstring name = L"CFloatingCube2_" + to_wstring(i);
 		CFloatingCube* cube = Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(name);
-		cube->Set_Loop();
-		//cube->SetTrigger(true);
 		cube->Set_Info(cube->Get_Component<CTransform>()->Get_Pos(), { 0.f, 1.f, 0.f }, 20.f, 5.f, 0.5f);
 	}
 
 	CFloatingCube* CFloatingCube2_1_1 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CFloatingCube2_1_1");
-	CFloatingCube2_1_1->Set_Info(CFloatingCube2_1_1->Get_Component<CTransform>()->Get_Pos(), { 0.f, -1.f, 0.f }, 25.f, 5.f, 0.5f);
-	//CFloatingCube2_1_1->SetTrigger(true);
+	CFloatingCube2_1_1->Set_Info(CFloatingCube2_1_1->Get_Component<CTransform>()->Get_Pos(), { 0.f, -1.f, 0.f }, 30.f, 3.f, 0.5f);
+	CFloatingCube2_1_1->Set_Loop();
+	CFloatingCube2_1_1->SetTrigger(true);
 }
 
 void SceneStage3::DirectionSet()
@@ -197,13 +197,13 @@ void SceneStage3::StairSet()
 void SceneStage3::Set_Triggers()
 {
 	// 첫번째 문
-	_bool bDoorTrigger1_1 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CPlayerTriggerCube>(L"CDoorTrigger1_1")->Get_InGate();
+	_bool bDoorTrigger1_1 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CZoneSensor>(L"CDoorTrigger1_1")->Get_SensorState();
 
 	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CDoor1_1")->Set_GoBack(!bDoorTrigger1_1);
 	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CDoor1_2")->Set_GoBack(!bDoorTrigger1_1);
 
-	// 두번째 문
-	_bool bDoorTrigger1_2 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CPlayerTriggerCube>(L"CDoorTrigger1_2")->Get_InGate();
+	// 두번째 문 
+	_bool bDoorTrigger1_2 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CZoneSensor>(L"CDoorTrigger1_2")->Get_SensorState();
 
 	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CDoor1_3")->Set_GoBack(!bDoorTrigger1_2);
 	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CDoor1_4")->Set_GoBack(!bDoorTrigger1_2);
@@ -260,6 +260,15 @@ void SceneStage3::Set_Triggers()
 			CMirrorSlotCube1_1->Set_Follow(false);
 		}
 	}
+
+
+	// 3개 가림막 풀기
+	// 1번째 가림막
+	_bool CPickSwitch2_1 = Get_Layer(LAYER_OBJECT)->Get_GameObject<CPickSwitch>(L"CPickSwitch2_1")->Get_SwitchState();
+	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CFloatingCube2_9")->Set_GoBack(!CPickSwitch2_1);
+	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CFloatingCube2_10")->Set_GoBack(!CPickSwitch2_1);
+	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CFloatingCube2_11")->Set_GoBack(!CPickSwitch2_1);
+	Get_Layer(LAYER_OBJECT)->Get_GameObject<CFloatingCube>(L"CFloatingCube2_12")->Set_GoBack(!CPickSwitch2_1);
 
 }
 
